7bccf58b0ee4a41fb2e10b9292478afbb3a281c6
MUTINDAH <petahmutinda@gmail.com>
2023-05-20 17:37:24 +0300

    AirBnB clone files

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """Base model class with common attributes and methods."""
+
+    id: str = None
+    created_at: datetime = None
+    updated_at: datetime = None
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        dict_ = self.__dict__.copy()
+        dict_["__class__"] = self.__class__.__name__
+        dict_["created_at"] = dict_["created_at"].isoformat()
+        dict_["updated_at"] = dict_["updated_at"].isoformat()
+        return dict_
