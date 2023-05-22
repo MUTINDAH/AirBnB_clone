4295062218cc902be9c457d80a3508bc378b6d35
MUTINDAH <petahmutinda@gmail.com>
2023-05-23 00:27:34 +0300

    AirBnB clone files

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-
 from datetime import datetime
 from uuid import uuid4
-
 
 
 class BaseModel:
@@ -12,20 +9,29 @@ class BaseModel:
     created_at: datetime = None
     updated_at: datetime = None
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        """Returns a dictionary containing all
+        keys/values of __dict__ of the instance."""
         dict_ = self.__dict__.copy()
         dict_["__class__"] = self.__class__.__name__
         dict_["created_at"] = dict_["created_at"].isoformat()
