31ca0abb840e65c5351979de8093497dff83510d
MUTINDAH <petahmutinda@gmail.com>
2023-05-24 00:24:32 +0300

    AirBnB clone files

--- console.py
+++ console.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
+""" Command interpreter console """
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB"""
-
+    '''Entry point of the command interpreter'''
     prompt = "(hbnb) "
 
+    def emptyline(self):
+        """ Ignore empty spaces """
+        pass
+
     def do_quit(self, arg):
-        """Exit the program"""
+        ''' Quit command to exit the program '''
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
-        print()  # Print a newline before exiting
+        ''' Exit the program '''
+        print()
         return True
-
-    def emptyline(self):
-        """Do nothing when an empty line is entered"""
-        pass
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
