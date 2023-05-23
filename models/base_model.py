6685205a4995f3cf2e8dc52e7700f3a25b9510a4
MUTINDAH <petahmutinda@gmail.com>
2023-05-23 16:11:35 +0300

    AirBnB clone files

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,16 @@
-from datetime import datetime
 from uuid import uuid4
+from datetime import datetime
+import json
 
 
 class BaseModel:
-    """Base model class with common attributes and methods."""
+    """Base model class for all other models."""
 
     id: str = None
     created_at: datetime = None
     updated_at: datetime = None
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -21,19 +21,35 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
-        """Returns a dictionary containing all
-        keys/values of __dict__ of the instance."""
+        """Returns a dictionary representation of the model."""
         dict_ = self.__dict__.copy()
-        dict_["__class__"] = self.__class__.__name__
-        dict_["created_at"] = dict_["created_at"].isoformat()
-        dict_["updated_at"] = dict_["updated_at"].isoformat()
+        dict_['__class__'] = self.__class__.__name__
+        dict_['created_at'] = dict_['created_at'].isoformat()
+        dict_['updated_at'] = dict_['updated_at'].isoformat()
         return dict_
+
+    def from_dict(self, dict_):
+        for key, value in dict_.items():
+            if key != "__class__":
+                setattr(self, key, value)
+        self.updated_at = datetime.now()
+
+
+if __name__ == "__main__":
+    from engine.file_storage import FileStorage
+
+    storage = FileStorage()
+    storage.reload()
+
+    model = BaseModel()
+    storage.new(model)
+    storage.save()
+    storage.reload()
+    print(storage.all())
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+from uuid import uuid4
+from datetime import datetime
+import json
+
+class FileStorage:
+    """File storage class for storing and retrieving models."""
+
+    __file_path: str = "file.json"
+    __objects: dict = {}
+
+    def __init__(self):
+        self.load()
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f, indent=4)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)."""
+        try:
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
