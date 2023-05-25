b31d23bb4f4285aa8ec4a668796f032606b4d7fa
MUTINDAH <petahmutinda@gmail.com>
2023-05-25 21:10:46 +0300

    AirBnB clone files

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
+import uuid
 from datetime import datetime
-from uuid import uuid4
-
-from models import storage
 
 
 class BaseModel:
-    """Base model class for all other classes."""
+    """Base class for all other models."""
 
     id: str = None
     created_at: datetime = None
     updated_at: datetime = None
 
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            if not storage.contains(self.id):
-                storage.new(self)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        storage.save(self)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary representation of the object."""
+        """Returns a dictionary representation of the model."""
         dict_ = self.__dict__.copy()
         dict_["__class__"] = self.__class__.__name__
         dict_["created_at"] = dict_["created_at"].isoformat()
