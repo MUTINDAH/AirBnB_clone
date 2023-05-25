20527c4a3d7b3ea9ff62eb2e56d592ca3943594f
MUTINDAH <petahmutinda@gmail.com>
2023-05-25 21:14:27 +0300

    AirBnB clone files

--- models/base_model.py
+++ models/base_model.py
@@ -4,28 +4,20 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all other models."""
-
-    id: str = None
-    created_at: datetime = None
-    updated_at: datetime = None
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary representation of the model."""
-        dict_ = self.__dict__.copy()
-        dict_["__class__"] = self.__class__.__name__
-        dict_["created_at"] = dict_["created_at"].isoformat()
-        dict_["updated_at"] = dict_["updated_at"].isoformat()
-        return dict_
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
